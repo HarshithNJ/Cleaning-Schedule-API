40f10b9942178d4b84b9a3bad3e0976ebfe40bef
Harshith N J <njharshith122@gmail.com>
2025-01-21 15:43:25 +0530

    project created

---
+++ src/test/java/org/cleaning/cleaning/CleaningApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cleaning.cleaning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleaningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
