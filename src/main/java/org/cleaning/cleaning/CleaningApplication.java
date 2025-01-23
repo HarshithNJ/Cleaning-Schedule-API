5513839587bb30244a5aa04bc25df9b196e1e4da
Harshith N J <njharshith122@gmail.com>
2025-01-23 17:48:04 +0530

    swagger installation

--- src/main/java/org/cleaning/cleaning/CleaningApplication.java
+++ src/main/java/org/cleaning/cleaning/CleaningApplication.java
@@ -3,7 +3,13 @@ package org.cleaning.cleaning;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Cleaning API", version = "1.0", description = "Design for managing the Cleaning tasks", 
+					contact = @Contact(name = "Harshith N J", email = "Harshith.dev2024@outlook.com")))
 public class CleaningApplication {
 
 	public static void main(String[] args) {
